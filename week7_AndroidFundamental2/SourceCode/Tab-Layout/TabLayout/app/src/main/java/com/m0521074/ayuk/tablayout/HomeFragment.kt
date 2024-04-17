97c4f245ebd026de19f0433e3a328909632f61bf
ningrom <sittiayuningrum@gmail.com>
2024-04-17 12:58:55 +0700

    done bg

---
+++ week7_AndroidFundamental2/SourceCode/Tab-Layout/TabLayout/app/src/main/java/com/m0521074/ayuk/tablayout/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.m0521074.ayuk.tablayout
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val tvLabel: TextView = view.findViewById(R.id.section_label)
+        val index = arguments?.getInt(ARG_SECTION_NUMBER, 0)
+        tvLabel.text = getString(R.string.content_tab_text, index)
+    }
+
+    companion object {
+        const val ARG_SECTION_NUMBER = "section_number"
+    }
+}
