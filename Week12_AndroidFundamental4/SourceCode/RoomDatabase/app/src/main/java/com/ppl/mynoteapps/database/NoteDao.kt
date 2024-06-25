51582a3e28321ce1d26e8ae581323edeb4f63beb
adiprasetya <adiprasetyaa11@gmail.com>
2024-06-25 22:50:52 +0700

    Source Code Room

---
+++ Week12_AndroidFundamental4/SourceCode/RoomDatabase/app/src/main/java/com/ppl/mynoteapps/database/NoteDao.kt
@@ -0,0 +1,21 @@
+package com.ppl.mynoteapps.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface NoteDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(note: Note)
+    @Update
+    fun update(note: Note)
+    @Delete
+    fun delete(note: Note)
+    @Query("SELECT * from note ORDER BY id ASC")
+    fun getAllNotes(): LiveData<List<Note>>
+}
---
+++ Week12_AndroidFundamental4/SourceCode/RoomDatabase/app/src/main/java/com/ppl/mynoteapps/ui/main/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.ppl.mynoteapps.ui.main
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ppl.mynoteapps.R
+import com.ppl.mynoteapps.databinding.ActivityMainBinding
+import com.ppl.mynoteapps.helper.ViewModelFactory
+import com.ppl.mynoteapps.ui.insert.NoteAddUpdateActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private var _activityMainBinding: ActivityMainBinding? = null
+    private val binding get() = _activityMainBinding
+
+    private lateinit var adapter: NoteAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        _activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        val mainViewModel = obtainViewModel(this@MainActivity)
+        mainViewModel.getAllNotes().observe(this) { noteList ->
+            if (noteList != null) {
+                adapter.setListNotes(noteList)
+            }
+        }
+
+        adapter = NoteAdapter()
+
+        binding?.rvNotes?.layoutManager = LinearLayoutManager(this)
+        binding?.rvNotes?.setHasFixedSize(true)
+        binding?.rvNotes?.adapter = adapter
+
+        binding?.fabAdd?.setOnClickListener {
+            val intent = Intent(this@MainActivity, NoteAddUpdateActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun obtainViewModel(activity: AppCompatActivity): MainViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(activity, factory).get(MainViewModel::class.java)
+    }
+}
